864af807b43af7f6665ff3c7c4bb0e4e27dcd293
Jan Rusell <janruselldev@gmail.com>
2026-02-16 16:27:37 +0800

    update services

--- apps/api/src/cadences/cadences.service.ts
+++ apps/api/src/cadences/cadences.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateCadenceDto } from './dto/create-cadence.dto';
+import { UpdateCadenceDto } from './dto/update-cadence.dto';
 
 @Injectable()
 export class CadencesService {
-  createCadence(): string {
-    return 'cadence created';
+  private cadences = new Map<string, CreateCadenceDto>();
+
+  createCadence(cadence: CreateCadenceDto) {
+    this.cadences.set(cadence.id, cadence);
+    return cadence;
   }
 
-  getCadence(id): string {
-    return `returned cadence ${id}`;
+  getCadence(id: string) {
+    return this.cadences.get(id);
   }
 
-  updateCadence(id): string {
-    return 'asdasdasd';
+  updateCadence(id: string, cadence: UpdateCadenceDto) {
+    const exists = this.cadences.get(id);
+    if (!exists) return null;
+
+    const updated = { ...exists, ...cadence };
+    this.cadences.set(id, updated);
+    return updated;
   }
 }
