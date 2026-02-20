af806a89797f6e90edc873dacf58a94e85e227b6
Jan Rusell <janruselldev@gmail.com>
2026-02-20 16:44:51 +0800

    remove generated files

--- apps/worker/src/workflow.d.ts
+++
@@ -1,5 +0,0 @@
-import { CadenceStep, WorkflowState, WorkflowInput } from "shared";
-export declare const getStateQuery: import("@temporalio/workflow").QueryDefinition<WorkflowState, [], string>;
-export declare const updateCadenceSignal: import("@temporalio/workflow").SignalDefinition<[CadenceStep[]], string>;
-export declare function cadenceWorkflow(input: WorkflowInput): Promise<String>;
-//# sourceMappingURL=workflow.d.ts.map
